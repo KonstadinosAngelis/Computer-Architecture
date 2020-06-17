2f05fe09417a29578f17ecc95134353c0653b7e6
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-06-17 13:47:00 -0700

    finished day 3 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,13 +1,23 @@
 """CPU functionality."""
 import sys
 
-program_file = "ls8\examples\mult.ls8"
+program_file = "ls8\examples\stack.ls8"
 
 class CPU:
     def __init__(self):
         self.ram = [0] * 256 #256 bytes of Memory
         self.reg = [0] * 8 #8 registers for quickly storing data
         self.pc = 0 #Progam counter for keeping a pointer of where we are in the program
+        self.sp = 7
+        self.reg[self.sp] = 0xf4
+        self.branch_table = {
+            0b00000001: self.halt,
+            0b10000010: self.ldi,
+            0b01000111: self.print,
+            0b10100010: self.multiply,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+        }
 
     def load(self, program):
         with open(program) as p:
@@ -27,52 +37,79 @@ class CPU:
 
                     address += 1
 
-    def alu(self, op, reg_a, reg_b):
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
-
     def ram_read(self, MAR): #Takes in a Memory Address Register (MAR) which is the position in the memory wanting to read
         return self.ram[MAR]
 
     def ram_write(self, MAR, MDR): #Takes in the MAR like above function. Also takes in Memory Data Register (MDR) which is the data getting writting to the MAR
         self.ram[MAR] = MDR
 
+    def halt(self):
+        self.pc += 1
+        sys.exit()
+
+    def ldi(self):
+        reg_num = self.ram_read(self.pc + 1)
+        val = self.ram_read(self.pc + 2)
+        self.reg[reg_num] = val
+        self.pc += 3
+
+    def print(self):
+        reg_num = self.ram_read(self.pc + 1)
+        print(self.reg[reg_num])
+        self.pc += 2
+
+    def multiply(self):
+        num1 = self.reg[self.ram_read(self.pc + 1)]
+        num2 = self.reg[self.ram_read(self.pc + 2)]
+
+        result = num1 * num2
+
+        self.reg[self.ram_read(self.pc + 1)] = result
+
+        self.pc += 3
+    
+    def push(self):
+        self.reg[self.sp] -= 1
+
+        reg_num = self.ram_read(self.pc + 1)
+        val = self.reg[reg_num]
+
+        stack_pos = self.reg[self.sp]
+
+        self.ram_write(stack_pos, val)
+
+        self.pc += 2
+
+    def pop(self):
+        pop_val = self.ram_read(self.reg[self.sp])
+
+        self.ram_write(self.reg[self.sp], 0)
+
+        self.reg[self.sp] += 1
+
+        reg_num = self.ram_read(self.pc + 1)
+        self.reg[reg_num] = pop_val
+
+        self.pc += 2
+        
     def run(self):
         running = True
-
         while running:
             ir = self.ram[self.pc]
-            if ir == 0b00000001: #HALT
-                running = False
-                self.pc += 1
 
-            elif ir == 0b10000010: #LDI
-                reg_num = self.ram_read(self.pc + 1)
-                val = self.ram_read(self.pc + 2)
-                self.reg[reg_num] = val
-                self.pc += 3
-
-            elif ir == 0b01000111: #Print
-                reg_num = self.ram_read(self.pc + 1)
-                print(self.reg[reg_num])
-                self.pc += 2
-            
-            elif ir == 0b10100010: #Mul
-                num1 = self.reg[self.ram_read(self.pc + 1)]
-                num2 = self.reg[self.ram_read(self.pc + 2)]
-
-                result = num1 * num2
-
-                self.reg[self.ram_read(self.pc + 1)] = result
-
-                self.pc += 3
+            if ir in self.branch_table:
+                self.branch_table[ir]()
 
             else:
                 print(f'Unknown Instruction {ir} at address {self.pc}')
                 sys.exit(1)
+
+    def alu(self, op, reg_a, reg_b):
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        #elif op == "SUB": etc
+        else:
+            raise Exception("Unsupported ALU operation")
 
 newCPU = CPU()
 newCPU.load(program_file)
